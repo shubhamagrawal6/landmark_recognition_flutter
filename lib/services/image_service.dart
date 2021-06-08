66ddfa6afaa3082dd2531379c319d3982ee4c56f
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-06-08 19:43:34 +0530

    image service to load and read as bytes

---
+++ lib/services/image_service.dart
@@ -0,0 +1,23 @@
+import 'dart:typed_data';
+
+import 'package:multi_image_picker/multi_image_picker.dart';
+
+class ImageService {
+  Future<Uint8List> loadImage() async {
+    var resultList = await MultiImagePicker.pickImages(
+      maxImages: 1,
+      enableCamera: true,
+    );
+    if (resultList.length == 0) {
+      return null;
+    } else {
+      return readAsBytes(resultList);
+    }
+  }
+
+  Future<Uint8List> readAsBytes(List<Asset> assets) async {
+    var asset = assets.single;
+    var byteData = await asset.getByteData();
+    return byteData.buffer.asUint8List();
+  }
+}
