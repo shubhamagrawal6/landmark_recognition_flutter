44ceec9e246139b71ae414c60dc541b004ee7433
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-06-08 19:43:51 +0530

    homepage designed

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:landmark_recognition_flutter/constants.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,13 +10,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  PageController _pageController;
-  double _currentPageValue = 0;
+  PageController _pageController = PageController(viewportFraction: 0.8);
+  double _currentPageValue = 0.0;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(viewportFraction: 0.8);
+    _pageController = PageController(
+      viewportFraction: 0.8,
+    )..addListener(
+        () => setState(() {
+          _currentPageValue = _pageController.page;
+        }),
+      );
   }
 
   @override
@@ -21,10 +31,93 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         extendBodyBehindAppBar: true,
         appBar: AppBar(
+          centerTitle: true,
           title: Text(
             "Select Continent",
             style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
           ),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    Constants.items[_currentPageValue.round()]['image'],
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(color: Colors.black.withOpacity(0.2)),
+              ),
+            ),
+            Center(
+              child: Container(
+                height: 450,
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: Constants.items.length,
+                  itemBuilder: (context, index) {
+                    var scale = (1 - (_currentPageValue - index).abs());
+                    return Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 5,
+                        vertical: 30 - 30 * scale,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: AssetImage(Constants.items[index]['image']),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 30,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.5),
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(15),
+                                bottomRight: Radius.circular(15),
+                              ),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  Constants.items[index]['title'],
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  Constants.items[index]['text'],
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
